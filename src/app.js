6dc5d67bd3e1d4348d2ddb6459a3bd7efca34c99
erickalemos <ericka.florentino@gmail.com>
2019-11-19 13:28:01 -0300

    atualizaçao do mapa

--- src/app.js
+++ src/app.js
@@ -5,24 +5,21 @@
 
 const platform = new H.service.Platform({
   'app_id': '0EeS3RWEaZacTmZVSe3r',
-  'apikey':'QBYz8rewc1RPYVM7jvaL9tGrICvd2vWFS-WVf_rvftI'
+  'apikey': 'QBYz8rewc1RPYVM7jvaL9tGrICvd2vWFS-WVf_rvftI'
   // 'apikey': 'b5q6ZKMFzd0A7Hmp-SWct71ZroHRsXLLl4r5WvJuhVg'
 
-  });
+});
 
-  // Obtain the default map types from the platform object:
+// Obtain the default map types from the platform object:
 const defaultLayers = platform.createDefaultLayers();
 
 // Instantiate (and display) a map object:
 const map = new H.Map(
   document.querySelector('.map-container'),
-  defaultLayers.vector.normal.map,
-  {
-    zoom: 15,
-    center: { lat: -23.557562, lng: -46.659315}
-  });
+  defaultLayers.vector.normal.map, 
+  {zoom: 16, center: {lat: -23.558822, lng: -46.662936 } })
 
-  // Add map events functionality to the map
+// Add map events functionality to the map
 const mapEvents = new H.mapevents.MapEvents(map);
 // Add behavior to the map: panning, zooming, dragging.
 const behavior = new H.mapevents.Behavior(mapEvents);
@@ -32,73 +29,71 @@ const ui = H.ui.UI.createDefault(map, defaultLayers);
 let icon = new H.map.Icon("images/beers.png");
 
 // // Create a marker using the previously instantiated icon:
-let barBelaSantos = new H.map.Marker({ lat: -23.556972, lng: -46.662528}, { icon: icon });
-barBelaSantos.setData('Bar Bela Santos')
-barBelaSantos.addEventListener('tap', (event) =>{
-    console.log(event.target.b); 
-    const bubble = new H.ui.InfoBubble(
-      event.target.b,
-      {
-        content: event.target.getData()
-      }
-    );
-    ui.addBubble(bubble);
-  }, false);
+let barBelaSantos = new H.map.Marker({lat: -23.556972, lng: -46.662528}, {icon: icon});
+barBelaSantos.setData("Bar Bela Santos")
+barBelaSantos.addEventListener('tap', (event) => {
+   console.log(event.target);
+  const bubble = new H.ui.InfoBubble(
+    event.target.b, {
+      content: event.target.getData()
+    }
+  );
+  ui.addBubble(bubble);
+}, false);
 
-let oMalleys = new H.map.Marker({ lat: -23.557958, lng: -46.665997}, { icon: icon });
-oMalleys.setData('Omalleys')
-oMalleys.addEventListener('tap', (event) =>{
-    console.log(event.target.b); 
-    const bubble = new H.ui.InfoBubble(
-      event.target.b,
-      {
-        content: event.target.getData()
-      }
-    );
-    ui.addBubble(bubble);
-  }, false);
-let bellaJau = new H.map.Marker({ lat: -23.556733,  lng: -46.665414}, { icon: icon });
-bellaJau.setData('Bella Jau')
-bellaJau.addEventListener('tap', (event) =>{
-    console.log(event.target.b); 
-    const bubble = new H.ui.InfoBubble(
-      event.target.b,
-      {
-        content: event.target.getData()
-      }
-    );
-    ui.addBubble(bubble);
-  }, false);
-let meGusta = new H.map.Marker({ lat: -23.559183,  lng: -46.664836}, { icon: icon });
-meGusta.setData('Me Gusta')
-meGusta.addEventListener('tap', (event) =>{
-    console.log(event.target.b); 
-    const bubble = new H.ui.InfoBubble(
-      event.target.b,
-      {
-        content: event.target.getData()
-      }
-    );
-    ui.addBubble(bubble);
-  }, false);
-let orangePoint = new H.map.Marker({ lat: -23.556447,  lng:  -46.663897}, { icon: icon }); 
-orangePoint.setData('Orange Point')
-orangePoint.addEventListener('tap', (event) =>{
-    console.log(event.target.b); 
-    const bubble = new H.ui.InfoBubble(
-      event.target.b,
-      {
-        content: event.target.getData()
-      }
-    );
-    ui.addBubble(bubble);
-  }, false);
+let oMalleys = new H.map.Marker({lat: -23.557958, lng: -46.665997}, {icon: icon});
+oMalleys.setData("O'malley's")
+oMalleys.addEventListener('tap', (event) => {
+  // console.log(event.target.b);
+  const bubble = new H.ui.InfoBubble(
+    event.target.b, {
+      content: event.target.getData()
+    }
+  );
+  ui.addBubble(bubble);
+}, false);
+
+let bellaJau = new H.map.Marker({lat: -23.556733, lng: -46.665414}, {icon: icon});
+bellaJau.setData("Bella Jau")
+bellaJau.addEventListener('tap', (event) => {
+   console.log(event.target);
+  const bubble = new H.ui.InfoBubble(
+    event.target.b, {
+      content: event.target.getData()
+    }
+  );
+  ui.addBubble(bubble);
+}, false);
+
+let meGusta = new H.map.Marker({lat: -23.559183,lng: -46.664836}, {icon: icon});
+meGusta.setData("Me Gusta")
+meGusta.addEventListener('tap', (event) => {
+  // console.log(event.target.b);
+  const bubble = new H.ui.InfoBubble(
+    event.target.b, {
+      content: event.target.getData()
+    }
+  );
+  ui.addBubble(bubble);
+}, false);
+
+let orangePoint = new H.map.Marker({lat: -23.556447, lng: -46.663897}, {icon: icon});
+orangePoint.setData("Orange Point")
+orangePoint.addEventListener('tap', (event) => {
+  // console.log(event.target.b);
+  const bubble = new H.ui.InfoBubble(
+    event.target.b, {
+      content: event.target.getData()
+    }
+  );
+  ui.addBubble(bubble);
+}, false);
 
 map.addObjects([barBelaSantos, oMalleys, bellaJau, meGusta, orangePoint]);
 
-	const homePage = () => {
-	window.location.hash = '';
-	const template = `
+const homePage = () => {
+  window.location.hash = '';
+  const template = `
 	<h1>Página inicial sem estar logado</h1>
 	${Button({
 		title:'ir pro login',
@@ -108,14 +103,13 @@ map.addObjects([barBelaSantos, oMalleys, bellaJau, meGusta, orangePoint]);
 		title:'ir pro cadastro',
 		onclick: mudarprocadastro
 	})}`;
-	return template;
+  return template;
 }
 
 const mudarprologin = () => {
-	location.hash = 'login';
+  location.hash = 'login';
 }
 
 const mudarprocadastro = () => {
-	location.hash = 'register';
+  location.hash = 'register';
 }
-
--- src/pages/mainpageNotlogged.js
+++ src/pages/mainpageNotlogged.js
@@ -1,14 +1,72 @@
 import Button from '../components/button.js'
 
-const confirmacaoIdade = () => {
-	let age = confirm("VOCÊ TEM MAIS DE 18 ANOS?");
-	if(age ===true){
-		console.log("muito bem")
-	}else{
-		console.log("Vá jogar LOL")
-	}
-}
-//confirmacaoIdade()
+// $(function() {
+//   /* caixa-confirmacao representa a id onde o caixa de confirmação deve ser criada no html */
+//   $( "#caixa-confirmacao" ).dialog({
+//     resizable: false,
+//     height:140,
+
+//     /* 
+//      * Modal desativa os demais itens da tela, impossibilitando interação com eles,
+//      * forçando usuário a responder à pergunta da caixa de confirmação
+//      */ 
+//     modal: true,
+
+//     /* Os botões que você quer criar */
+//     buttons: {
+//       "Sim": function() {
+//         $( this ).dialog( "close" );
+//         alert("Você clicou em Sim!");
+//       },
+//       "Não": function() {
+//         $( this ).dialog( "close" );
+//         alert("Você clicou em Não");
+//       }
+//     }
+//   });
+// });
+
+// const confirmacaoIdade = () => {
+// 	let age = confirm("VOCÊ TEM MAIS DE 18 ANOS?");
+// 	if(age ===true){
+// 		console.log("muito bem")
+// 	}else{
+// 		console.log("Vá jogar LOL")
+// 	}
+// }
+// confirmacaoIdade()
+
+// const confirmacao = () => {
+//  let template = `<div id= "dialog-confirm" title="Executar função?"></div>
+//   ` 
+// }
+// addEventListener.
+// <button id="btn" onclick="confirmar();">Clica</button>
+// <div id="dialog-confirm" title="Executar função?"></div>
+
+// function funcao_b() {
+//   alert('funcao B');
+// }
+// function confirmar() {
+//     $( "#dialog-confirm" ).dialog({
+//       resizable: false,
+//       height: "auto",
+//       width: 400,
+//       modal: true,
+//       buttons: {
+//         "Yap executa função b": function() {
+//           $( this ).dialog( "close" );
+//           funcao_b();
+//         },
+//         'Confirmar': function() {
+//           $( this ).dialog( "close" );
+//           console.log('cancelado');
+//         }
+//       }
+//     });
+// }
+
+
 
 const db = firebase.firestore();
   db.collection('bars').get().then((snap) => {
