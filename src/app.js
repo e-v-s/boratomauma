8594810c4a7445305dad04add09073ee7b51a9ae
erickalemos <ericka.florentino@gmail.com>
2019-11-18 22:42:56 -0300

    Atualização do Bubble

--- src/app.js
+++ src/app.js
@@ -21,96 +21,80 @@ const map = new H.Map(
     zoom: 15,
     center: { lat: -23.557562, lng: -46.659315}
   });
-//   let onResult = function(result) {
-//     let locations = result.Response.View[0].Result,
-//       position,
-//       marker;
-//     // Add a marker for each location found
-//     for (i = 0;  i < locations.length; i++) {
-//     position = {
-//       lat: locations[i].Location.DisplayPosition.Latitude,
-//       lng: locations[i].Location.DisplayPosition.Longitude
-//     };
-//     map.setCenter(position)
-//     marker = new H.map.Marker(position);
-//     map.addObject(marker);
-    
-//     }
-//   };
 
-// // Get an instance of the geocoding service:
-// let geocoder = platform.getGeocodingService();
-
-// // Call the geocode method with the geocoding parameters,
-// // the callback and an error callback function (called if a
-// // communication error occurs):
-// geocoder.geocode(geocodingParams, onResult, function(e) {
-//   alert(e);
-// });
-
-// Create a marker icon from an image URL:
-
-
-// let icon = new H.map.Icon("images/beers.png");
-
-// // Create a marker using the previously instantiated icon:
-// let barBelaSantos = new H.map.Marker({ lat: -23.556972, lng: -46.662528}, { icon: icon });
-// let oMalleys = new H.map.Marker({ lat: -23.557958, lng: -46.665997}, { icon: icon });
-// let bellaJau = new H.map.Marker({ lat: -23.556733,  lng: -46.665414}, { icon: icon });
-// let meGusta = new H.map.Marker({ lat: -23.559183,  lng: -46.664836}, { icon: icon });
-// let orangePoint = new H.map.Marker({ lat: -23.556447,  lng:  -46.663897}, { icon: icon });  
-//     // Add the marker to the map:
-// // map.addObject([barBelaSantos, oMalleys, bellaJau, meGusta, orangePoint]);
-// map.addObject(barBelaSantos);
-// map.addObject(oMalleys);
-// map.addObject(bellaJau);
-// map.addObject(meGusta);
-// map.addObject(orangePoint);
-
-// Add map events functionality to the map
+  // Add map events functionality to the map
 const mapEvents = new H.mapevents.MapEvents(map);
 // Add behavior to the map: panning, zooming, dragging.
 const behavior = new H.mapevents.Behavior(mapEvents);
 
 const ui = H.ui.UI.createDefault(map, defaultLayers);
-const markerOne = new H.map.Marker({lat: -23.557562, lng: -46.659315});
-markerOne.setData(`<p>Bar do ...</p>`);
-markerOne.addEventListener('tap', event =>{ 
-  const bubble = new H.ui.UI.InfoBubble(
-    event.target.getPosition(),
-    {
-      content: event.target.getData()
-    }
-  );
-  ui.addBubble(bubble);
-}, false);
-markerTwo = new H.map.Marker({lat: -23.556447,  lng:  -46.663897});
-markerTwo.setData(`<p>Bar do ...</p>`);
-markerTwo.addEventListener("tap", event =>{ 
-  const bubble = new H.ui.UI.InfoBubble(
-    event.target.getPosition(),
-    {
-      content: event.target.getData()
-    }
-  );
-  ui.addBubble(bubble);
-}, false);
-map.addObject(markerOne);
-map.addObject(markerTwo);
 
+let icon = new H.map.Icon("images/beers.png");
 
-// barBelaSantos = new H.map.Marker ({ lat: -23.556972, lng: -46.662528});
-// barBelaSantos.setData(`<p>Bar do ...</p>`);
-// barBelaSantos.addEventListener("tap", event =>{ 
-//   const bubble = new H.ui.UI.InfoBubble(
-//     event.target.getPosition(),
-//     {
-//       content: event.target.getData()
-//     }
-//   );
-//   ui.addBubble(bubble);
-// }, false);
-// map.addObject(barBelaSantos);
+// // Create a marker using the previously instantiated icon:
+let barBelaSantos = new H.map.Marker({ lat: -23.556972, lng: -46.662528}, { icon: icon });
+barBelaSantos.setData('Bar Bela Santos')
+barBelaSantos.addEventListener('tap', (event) =>{
+    console.log(event.target.b); 
+    const bubble = new H.ui.InfoBubble(
+      event.target.b,
+      {
+        content: event.target.getData()
+      }
+    );
+    ui.addBubble(bubble);
+  }, false);
+
+let oMalleys = new H.map.Marker({ lat: -23.557958, lng: -46.665997}, { icon: icon });
+oMalleys.setData('Omalleys')
+oMalleys.addEventListener('tap', (event) =>{
+    console.log(event.target.b); 
+    const bubble = new H.ui.InfoBubble(
+      event.target.b,
+      {
+        content: event.target.getData()
+      }
+    );
+    ui.addBubble(bubble);
+  }, false);
+let bellaJau = new H.map.Marker({ lat: -23.556733,  lng: -46.665414}, { icon: icon });
+bellaJau.setData('Bella Jau')
+bellaJau.addEventListener('tap', (event) =>{
+    console.log(event.target.b); 
+    const bubble = new H.ui.InfoBubble(
+      event.target.b,
+      {
+        content: event.target.getData()
+      }
+    );
+    ui.addBubble(bubble);
+  }, false);
+let meGusta = new H.map.Marker({ lat: -23.559183,  lng: -46.664836}, { icon: icon });
+meGusta.setData('Me Gusta')
+meGusta.addEventListener('tap', (event) =>{
+    console.log(event.target.b); 
+    const bubble = new H.ui.InfoBubble(
+      event.target.b,
+      {
+        content: event.target.getData()
+      }
+    );
+    ui.addBubble(bubble);
+  }, false);
+let orangePoint = new H.map.Marker({ lat: -23.556447,  lng:  -46.663897}, { icon: icon }); 
+orangePoint.setData('Orange Point')
+orangePoint.addEventListener('tap', (event) =>{
+    console.log(event.target.b); 
+    const bubble = new H.ui.InfoBubble(
+      event.target.b,
+      {
+        content: event.target.getData()
+      }
+    );
+    ui.addBubble(bubble);
+  }, false);
+
+map.addObjects([barBelaSantos, oMalleys, bellaJau, meGusta, orangePoint]);
 
 	const homePage = () => {
 	window.location.hash = '';
